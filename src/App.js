2821d49780d39e22832a6fbf8a65fd34cea27541
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2022-12-01 10:08:04 -0300

    feat: props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Post from './Post';
 
 export default function App() {
     return (
         <React.Fragment>
             <h1>Componente App</h1>
+            <Post title="Título da notícia" subtitle="Subtítulo da notícia"></Post>
+            <Post title="Título da notícia" subtitle="Subtítulo da notícia"></Post>
+
         </React.Fragment>
     );
 }
