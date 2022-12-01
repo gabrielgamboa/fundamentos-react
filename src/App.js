91213fcf207d5953503a5f5a292039a6bc5db524
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2022-12-01 14:13:48 -0300

    feat: learning about children props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Post from './Post';
+import Header from './Header';
 
 export default function App() {
     return (
         <React.Fragment>
-            <h1>Componente App</h1>
+           <Header title="Jstack Blog">
+                <h2>Posts da semana</h2>
+           </Header>
+
+           <hr/>
+
             <Post title="Título da notícia" subtitle="Subtítulo da notícia"></Post>
             <Post title="Título da notícia" subtitle="Subtítulo da notícia"></Post>
 
