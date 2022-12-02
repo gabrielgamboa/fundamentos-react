4a791011847df6d09507b43a420826086e61fb3f
Gabriel Gamboa <gabriel.gamboa@mblabs.com.br>
2022-12-02 19:22:53 -0300

    feat: learn how to use useState function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './Post';
 import Header from './Header';
 
 export default function App() {
-    const posts = [
+    const [posts, setPost] = useState([
         {
+            id: Math.random(),
             title: 'Titulo 1',
-            subtitle: 'Subtitulo',
+            subtitle: 'Subtitulo 1',
             likes: 30,
         },
         {
+            id: Math.random(),
             title: 'Titulo 2',
             subtitle: 'Subtitulo 2',
             likes: 40,
         },
-    ]
+    ]);
+
+    function handleRefresh() {
+        setPost((prevState) => [...prevState, {
+            id: Math.random(),
+            title: `Titulo ${posts.length}`,
+            subtitle: `Subtitulo ${posts.length}`,
+            likes: 50,
+        }]);
+
+        //Diferente do exemplo acima, se não utilizarmos a função com o prevState
+        //Poderá haver inconsistência de dados devido à atualização de componentes serem feitas de 
+        //Forma assíncrona
+
+        //Normalmente passamos diretamente quando a atualização não depende do estado anterior
+
+        // setPost([...posts, {
+        //     id: Math.random(),
+        //     title: `Titulo ${posts.length}`,
+        //     subtitle: `Subtitulo ${posts.length}`,
+        //     likes: 50,
+        // }]);
+    }
 
     return (
-        <React.Fragment>
+        <>
             <Header title="Jstack Blog">
-                <h2>Posts da semana</h2>
+                <h2>
+                    Posts da semana
+                    <button onClick={handleRefresh}>Atualizar</button>
+                </h2>
             </Header>
 
             <hr />
 
             {posts.map(post => (
                 <Post
-                    key={post.subtitle}
+                    key={post.id}
                     title={post.title}
                     subtitle={post.subtitle}
                     likes={post.likes}
@@ -34,6 +61,6 @@ export default function App() {
                 </Post>
             ))}
 
-        </React.Fragment>
+        </>
     );
 }
